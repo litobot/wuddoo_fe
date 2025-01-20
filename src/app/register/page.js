de6cadaefbbc8612ba7a4c8b0ed2d76432d06bec
Lito Croy <146680987+litobot@users.noreply.github.com>
2025-01-20 15:04:47 -0700

    Add form fields with labels

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -2,6 +2,24 @@ export default function RegisterPage() {
   return (
     <div>
       <h1>Welcome!</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" required />
+        </div>
+        <div>
+          <label htmlFor="passwordConfirmation">Password Confirmation:</label>
+          <input type="text" id="passwordConfirmation" name="passwordConfirmation" required />
+        </div>
+      </form>
     </div>
   );
 }
