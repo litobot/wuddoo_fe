e3c900492239e27435a0e48adca0b67aef53d2e6
Lito Croy <146680987+litobot@users.noreply.github.com>
2025-01-20 17:03:54 -0700

    Add handleFormSubmit function for User's data

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,24 +1,61 @@
+"use client" // Apparently we have to add this otherwise Next.js doesn't know to accept the User's form inputs
+
 export default function RegisterPage() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const userData = new FormData(event.target);
+    const data = Object.fromEntries(userData.entries());  // Turns the User's input data into an object where the key == the form name and the value == the User's input
+
+    // Confirm the passwords match
+    if (data.password !== data.passwordConfirmation) {
+      alert("Passwords must match!");
+      return;
+    }
+
+    console.log("Form submitted:", data);
+  }
+
   return (
     <div>
       <h1>Welcome!</h1>
-      <form>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            required 
+          />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="text" id="email" name="email" required />
+          <input 
+            type="text" 
+            id="email" 
+            name="email" 
+            required 
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="text" id="password" name="password" required />
+          <input 
+            type="text" 
+            id="password" 
+            name="password" 
+            required 
+          />
         </div>
         <div>
           <label htmlFor="passwordConfirmation">Password Confirmation:</label>
-          <input type="text" id="passwordConfirmation" name="passwordConfirmation" required />
+          <input 
+            type="text" 
+            id="passwordConfirmation" 
+            name="passwordConfirmation" 
+            required 
+          />
         </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
